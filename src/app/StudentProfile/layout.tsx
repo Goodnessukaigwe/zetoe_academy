f963ab36fc9bfda46251ba9c5dcf3daa45c2a5b9
kosisolaz <ogbozorkosiso55@gmail.com>
2025-12-14 15:46:32 +0100

    Add StudentProfile layout, navbar, and page

---
+++ src/app/StudentProfile/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './navbar';
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#f2f2f2] font-['Open_Sans']">
+    
+      <Navbar />
+
+      <main className="flex-1 p-6">{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
